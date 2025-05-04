d7b360780e8d89a07e1f7686d7501575a904ab3f
Brook-Soul-King <lukas.schrooer.25@gmail.com>
2025-05-04 17:10:34 +0200

    implement Game json in game.ts

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game!: Game;
 
-  constructor() {}
+  constructor() { }
 
-  takeCard(){
-    this.pickCardAnimation = true;
+  ngOnInit(): void {
+    this.newGame();
   }
 
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
